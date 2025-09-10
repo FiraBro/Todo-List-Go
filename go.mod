be769229371aa27f882047838e67e5867553643e
Firagos <jemalfiragos@gmail.com>
2025-09-10 10:22:02 +0300

    creating the frist commit using go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module Todo-List-Go
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	var name string = "fira"
+	fmt.Println("Name:", name)
+}
