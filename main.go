aa6016c4fe6fda5aa2da2e608da8ec9be28d559a
Firagos <jemalfiragos@gmail.com>
2025-09-25 07:50:18 +0300

    larning for loop in go

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
-
+func prossesTracking( ) error {
+	fmt.Println("Process Tracking")
+	names := []string{"Alice", "Bob", "Charlie"}
+	for index, value := range names {
+		fmt.Printf("the valeu of index %d is %s\n", index, value)
+	}
+	return errors.New("Process Tracking Error")
+}
 func main() {
-	fmt.Println("Hello, World!")
-	var name string = "fira"
-	fmt.Println("Name:", name)
+	prossesTracking()
+	
 }
