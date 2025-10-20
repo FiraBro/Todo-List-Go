559b79fa20bed6499d0a2041d6bbd699125b6884
Firagos <jemalfiragos@gmail.com>
2025-10-20 14:38:56 +0300

    debuging for long time

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"Todo-List-Go/internal/store"
 	"log"
 	"net/http"
 	"time"
@@ -8,32 +9,38 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
-type application struct{
+
+type application struct {
 	config config
+	store  store.Storage // or *store.PostgreSQLStorage depending on your code
 }
 
-type config struct{
+type config struct {
 	addr string
 }
-func(app *application) mount() * chi.Mux {
-		r := chi.NewRouter()
-		// Add middleware
+
+func (app *application) mount() *chi.Mux {
+	r := chi.NewRouter()
+
+	// Add middleware
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.RequestID)
-		
 	r.Use(middleware.Logger)
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
+
 	return r
 }
-func (app *application) run(mux * chi.Mux) error{
-	srv:=&http.Server{
-		Addr: app.config.addr,
-		Handler: mux,
-		WriteTimeout: time.Second * 30,
-		ReadTimeout: time.Second * 10,
+
+func (app *application) run(mux *chi.Mux) error {
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout:  10 * time.Second,
 	}
-	log.Printf("server is running %s",app.config.addr)
+	log.Printf("Server is running on %s", app.config.addr)
 	return srv.ListenAndServe()
 }
