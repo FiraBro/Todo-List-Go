b5f2b89a8c43ba5485e1b776782bbf37f1be3043
Firagos <jemalfiragos@gmail.com>
2025-10-21 14:40:54 +0300

    creating a user and finishing it

--- internal/store/users.go
+++ internal/store/users.go
@@ -4,10 +4,29 @@ import (
 	"context"
 	"database/sql"
 )
+type user struct{
+Name string `json:"name"`
+Age int8 `json:"age"`
+Password string `json:"password"`
+CreatedAt string `json:"created_at"`
+UpdatedAt string `json:"updated_at"`
+
+}
 type userStore struct{
 	db *sql.DB
 }
 
-func (s *userStore) Create(ctx context.Context) error {
+func (s *userStore) Create(ctx context.Context,user *user) error {
+	query:=`
+	INSERT INTO user (user.name,user.password,user.age)
+	VALUES ($1, $2, $3)
+	RETURNING id, created_at, updated_at
+	`
+	err:=s.db.QueryRowContext(ctx,query,user.Name,user.Age,user.Password).Scan(
+		&user.CreatedAt,
+		&user.UpdatedAt)
+	if err !=nil{
+		return err
+	}
 	return nil
 }
