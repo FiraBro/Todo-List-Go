347ad75c94edbde36a99e39097b5eb4a0b9389c8
Firagos <jemalfiragos@gmail.com>
2025-10-21 14:40:26 +0300

    creating a post and finishing it

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -5,10 +5,33 @@ import (
 	"database/sql"
 )
 
+type post struct{
+	ID string `json:"id"`
+	Title string `json:"title"`
+	Content string `json:"content"`
+	UserID int64 `json:"user_id"`
+	// Tags []string `json:"tags"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+}
 type postsSoter struct{
 	db *sql.DB
 }
 
-func (s *postsSoter) Create(ctx context.Context) error {
+
+func (s *postsSoter) Create(ctx context.Context,post *post) error {
+	query:=`
+	INSERT INTO post (title, content, user_id)
+	VALUES ($1, $2, $3)
+	RETURNING id, created_at, updated_at
+	`
+	err:= s.db.QueryRowContext(ctx, query, post.Title, post.Content, post.UserID).Scan(
+		&post.ID,
+		&post.CreatedAt,
+		&post.UpdatedAt,
+	)
+	if err != nil{
+		return err
+	}
 	return nil
 }
