f9b6de4fc7b466ebf5417483ead1088cca109d6d
Firagos <jemalfiragos@gmail.com>
2025-10-21 15:00:42 +0300

    modifing it

--- internal/store/users.go
+++ internal/store/users.go
@@ -4,10 +4,11 @@ import (
 	"context"
 	"database/sql"
 )
-type user struct{
-Name string `json:"name"`
+type users struct{
+ID int8 `json:"id"`
+Username string `json:"username"`
 Age int8 `json:"age"`
-Password string `json:"password"`
+Password string `json:"_"`
 CreatedAt string `json:"created_at"`
 UpdatedAt string `json:"updated_at"`
 
@@ -16,15 +17,15 @@ type userStore struct{
 	db *sql.DB
 }
 
-func (s *userStore) Create(ctx context.Context,user *user) error {
+func (s *userStore) Create(ctx context.Context,users *users) error {
 	query:=`
-	INSERT INTO user (user.name,user.password,user.age)
+	INSERT INTO user (user.username,user.password,user.age)
 	VALUES ($1, $2, $3)
 	RETURNING id, created_at, updated_at
 	`
-	err:=s.db.QueryRowContext(ctx,query,user.Name,user.Age,user.Password).Scan(
-		&user.CreatedAt,
-		&user.UpdatedAt)
+	err:=s.db.QueryRowContext(ctx,query,users.Username,users.Age,users.Password).Scan(
+		&users.CreatedAt,
+		&users.UpdatedAt)
 	if err !=nil{
 		return err
 	}
