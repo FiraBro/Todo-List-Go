abbc8f82dfec0f7b216050f1ba5b9e478c180109
Firagos Jemal <jemalfiragos@gmail.com>
2025-11-05 00:10:22 +0300

    fixing the error

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"log"
+
 	"Todo-List-Go/internal/db"
-	"context"
 	"Todo-List-Go/internal/env"
 	"Todo-List-Go/internal/store"
-	"log"
-	"time"
 )
 
+// DB config
 type dbConfig struct {
 	addr         string
 	maxOpenConns int
@@ -16,34 +16,40 @@ type dbConfig struct {
 	maxIdleTime  string
 }
 
+// App config
 type config struct {
 	addr string
 	db   dbConfig
 }
 
 func main() {
+	// Load config from env or defaults
 	cfg := config{
 		addr: env.GetString("ADDR", ":4000"),
 		db: dbConfig{
 			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/social?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
-			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"), // string, not int
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
 		},
 	}
 
+	// Connect to DB
 	dbConn, err := db.New(cfg.db.addr, cfg.db.maxIdleConns, cfg.db.maxIdleTime, cfg.db.maxOpenConns)
 	if err != nil {
 		log.Panic(err)
 	}
 
+	// Initialize store
 	store := store.NewSQLStorage(dbConn)
 
+	// Create application
 	app := &application{
 		config: cfg,
 		store:  store,
 	}
 
+	// Start server
 	mux := app.mount()
 	log.Fatal(app.run(mux))
 }
